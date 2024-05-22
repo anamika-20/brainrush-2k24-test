d26b4cdc4366fc9fae171f5c770e216cd2a83f88
anamika-20 <anamikatiwary20@gmail.com>
2024-05-22 21:57:36 +0530

    quiz frontend work

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Button from "@components/Button";
 
 const page = () => {
   const time = 60;
@@ -34,146 +36,130 @@ const page = () => {
   };
 
   const buttons = ["Paris", "Madrid", "Berlin", "Rome"];
+
+  const [selectedTopics, setSelectedTopics] = useState({
+    topic1: "",
+    topic2: "",
+    topic3: "",
+  });
+
+  const options = [
+    { value: "aptitude", label: "Aptitude" },
+    { value: "math", label: "Maths" },
+    { value: "movies", label: "Movies" },
+  ];
+
+  const handleSelectChange = (event, topic) => {
+    const newSelectedTopics = {
+      ...selectedTopics,
+      [topic]: event.target.value,
+    };
+    setSelectedTopics(newSelectedTopics);
+  };
+
+  const filterOptions = (exclude) =>
+    options.filter((option) => !exclude.includes(option.value));
+
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col mx-5 items-center justify-center ">
       <div
         className="max-w-md w-full p-6 bg-white rounded-lg"
-        style={{ height: "70%", width: "85%" }}
+        style={{ width: "90%", height: "700px" }}
       >
-        {/* {formatTime(timeLeft)} */}
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-3xl text-background font-bold">QUIZ</h1>
-          <div className="flex items-center text-sm text-background">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 mr-1"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+          <h1 className="text-3xl text-background font-bold">Round-1</h1>
+        </div>
+        {/* Options  */}
+        <div>
+          <div className="mb-6 h-1 w-full bg-bgGray ">
+            <div className="h-1 bg-background " style={{ width: "100%" }}></div>
+          </div>
+
+          <h3 className="text-left text-background font-bold">
+            CHOOSE TOPIC 1
+          </h3>
+          <div
+            className="border-2 border-background rounded-md"
+            style={{ padding: "9px", margin: "5px 0" }}
+          >
+            <select
+              className="w-full py-3 bg-white-600 px-3 font-bold rounded-md"
+              name="topic1"
+              value={selectedTopics.topic1}
+              onChange={(e) => handleSelectChange(e, "topic1")}
             >
-              <path
-                fillRule="evenodd"
-                d="M10 2a8 8 0 100 16 8 8 0 000-16zM9 10V5a1 1 0 012 0v4h3a1 1 0 010 2H9a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-            {formatTime(timeLeft)}
+              <option value="">Select Topic 1</option>
+              {filterOptions([
+                selectedTopics.topic2,
+                selectedTopics.topic3,
+              ]).map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <h3 className="text-left text-background font-bold">
+            CHOOSE TOPIC 2
+          </h3>
+          <div
+            className="border-2 border-background rounded-md"
+            style={{ padding: "9px", margin: "5px 0" }}
+          >
+            <select
+              className="w-full py-3 bg-white-600 px-3 font-bold rounded-md"
+              name="topic2"
+              value={selectedTopics.topic2}
+              onChange={(e) => handleSelectChange(e, "topic2")}
+            >
+              <option value="">Select Topic 2</option>
+              {filterOptions([
+                selectedTopics.topic1,
+                selectedTopics.topic3,
+              ]).map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <h3 className="text-left  text-background font-bold">
+            CHOOSE TOPIC 3
+          </h3>
+          <div
+            className="border-2 border-background rounded-md"
+            style={{ padding: "9px", margin: "5px 0" }}
+          >
+            <select
+              className="w-full py-3 bg-white-600 px-3 font-bold rounded-md"
+              name="topic3"
+              value={selectedTopics.topic3}
+              onChange={(e) => handleSelectChange(e, "topic3")}
+            >
+              <option value="">Select Topic 3</option>
+              {filterOptions([
+                selectedTopics.topic1,
+                selectedTopics.topic2,
+              ]).map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
-        {/* Progress bar */}
-        <div className="mb-6 h-1 w-full bg-bgGray ">
-          <div
-            className="h-1 bg-background "
-            style={{ width: `${widthPercentage}%` }}
-          ></div>
-        </div>
-        {/* Question and review section */}
-        <div className="flex justify-between items-center text-background text-sm mb-4">
-          <span>1 of 25 Questions</span>
-          <label className="flex items-center">
-            <input
-              type="checkbox"
-              className="mr-2 accent-background"
-              style={{
-                width: "16px",
-                height: "16px",
-              }}
-            />
-            Mark for Review
-          </label>
-        </div>
-
-        {/* Question */}
-        <h2 className="text-lg font-medium mb-4 text-gray-800">
-          What is the capital of France?What is the capital of France?What is
-          the capital of France?What is the capital of France?What is the
-          capital of France?What is the capital of France?What is the capital of
-          France?
-        </h2>
-        {/* Options */}
-        <div className="grid grid-cols-1 gap-4">
-          {buttons.map((city, index) => (
-            <button
-              key={index}
-              onClick={() => handleClick(index)}
-              className={`flex items-center justify-between border py-2 px-4 rounded-md w-full focus:outline-none 
-      ${
-        clickedIndex === index
-          ? "border-background text-background"
-          : "border-gray-300 text-gray-800"
-      }
-     `}
-            >
-              {city}
-              {clickedIndex === index ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 ml-2"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <circle
-                    cx="10"
-                    cy="10"
-                    r="10"
-                    fill="currentColor"
-                    className="text-background"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 00-1.414-1.414L7 12.586 4.707 10.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l9-9z"
-                    clipRule="evenodd"
-                    className="text-white"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 ml-2"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <circle cx="10" cy="10" r="9" />
-                </svg>
-              )}
-            </button>
-          ))}
-        </div>
-        {/* Navigation buttons */}
-        <div className="flex justify-between mt-[22px]  space-x-4">
-          <button className="flex items-center justify-center border border-gray-300 py-2 px-4 rounded-md w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 mr-2"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.707 4.293a1 1 0 010 1.414L5.414 8H15a1 1 0 110 2H5.414l2.293 2.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-            Previous
-          </button>
-          <button className="flex items-center justify-center border border-background bg-background text-white py-2 px-4 rounded-md w-full">
-            Next
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 ml-2"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.293 15.707a1 1 0 010-1.414L14.586 12H5a1 1 0 110-2h9.586l-2.293-2.293a1 1 0 111.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
+        <button
+          className="mt-15 flex items-center justify-center border border-background bg-background text-white py-2 px-4 rounded-md w-full mt-4 "
+          onClick={() => {
+            router.push("/type1");
+          }}
+        >
+          SUBMIT
+        </button>
       </div>
+      <div></div>
     </div>
   );
 };
